7d5c20948924cb7c6b041f9eb614eca190cf6ab5
Adam Brzakala <adam.brz@onet.eu>
2023-02-13 00:46:42 +0100

    Add example header

---
+++ libs/crypto/include/yabil/crypto/crypto.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace yabil::crypto
+{
+
+int crypto_test();
+
+}  // namespace yabil::crypto
--- libs/crypto/src/crypto.cpp
+++ libs/crypto/src/crypto.cpp
@@ -1,4 +1,11 @@
+#include <yabil/crypto/crypto.h>
+
+namespace yabil::crypto
+{
+
 int crypto_test()
 {
     return 0;
 }
+
+}  // namespace yabil::crypto
