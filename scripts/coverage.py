d0b37e4a6039ffc038830d306fd340779baeea2e
Andrew2a1 <adam.brz@onet.eu>
2023-03-12 19:40:57 +0100

    Improve coverage

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -1,27 +1,44 @@
 import os
 import glob
-import shutil
+import multiprocessing
 import subprocess
 
-from argparse import ArgumentParser
 from build import conan_install, cmake_config, cmake_build
+from argparse import ArgumentParser
+
+def generate_coverage_report(llvm_profdata, llvm_cov):
+    source_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    build_type = "Debug"
+    build_dir = f"{source_dir}/build/coverage"
+
+    conan_install(source_dir, "clang", build_type, build_dir)
+    cmake_config("coverage")
+    cmake_build(build_dir, build_type)
+
+    subprocess.check_call(
+        f"ctest -C Debug -j {multiprocessing.cpu_count()}",
+        shell=True,
+        cwd=build_dir,
+    )
+
+    binary_dir = f"{build_dir}/bin"
+    lib_dir = f"{build_dir}/lib"
+    output_dir = f"{build_dir}/coverage_report"
+
+    merge_raw_profiles(llvm_profdata, binary_dir)
+    generate_html(llvm_cov, source_dir, binary_dir, lib_dir, output_dir)
 
 
-def generate_coverage_report(src_dir, binary_dir, lib_dir, output_dir):
-    merge_raw_profiles(binary_dir)
-    generate_html(src_dir, binary_dir, lib_dir, output_dir)
-
-
-def merge_raw_profiles(binary_dir):
+def merge_raw_profiles(llvm_profdata, binary_dir):
     raw_profiles = glob.glob(f"{binary_dir}/*.profraw")
     subprocess.check_call(
-        f"llvm-profdata merge -sparse {' '.join(raw_profiles)} -o coverage.profdata",
+        f"{llvm_profdata} merge -sparse {' '.join(raw_profiles)} -o coverage.profdata",
         shell=True,
         cwd=binary_dir,
     )
 
 
-def generate_html(src_dir, binary_dir, lib_dir, output_dir):
+def generate_html(llvm_cov, src_dir, binary_dir, lib_dir, output_dir):
     source_files = glob.glob(f"{src_dir}/**/.cpp", recursive=True) + glob.glob(
         f"{src_dir}/**/.h", recursive=True
     )
@@ -32,24 +49,34 @@ def generate_html(src_dir, binary_dir, lib_dir, output_dir):
     source_flags = ["-sources " + file for file in source_files]
 
     subprocess.check_call(
-        f"llvm-cov show -format=html -output-dir={output_dir} -instr-profile coverage.profdata {' '.join(object_flags)} {' '.join(source_flags)}",
+        f"{llvm_cov} show -format=html -output-dir={output_dir} -instr-profile coverage.profdata {' '.join(object_flags)} {' '.join(source_flags)}",
+        shell=True,
+        cwd=binary_dir,
+    )
+
+    subprocess.check_call(
+        f"{llvm_cov} report -show-region-summary=false -show-functions=false -instr-profile coverage.profdata {' '.join(object_flags)} {' '.join(source_flags)}",
         shell=True,
         cwd=binary_dir,
     )
 
 
 if __name__ == "__main__":
-    source_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    build_type = "Debug"
-    build_dir = f"{source_dir}/build/coverage"
-
-    conan_install(source_dir, "clang", build_type, build_dir)
-    cmake_config("coverage")
-    cmake_build(build_dir, build_type)
-
-    generate_coverage_report(
-        source_dir,
-        f"{build_dir}/bin",
-        f"{build_dir}/lib",
-        f"{build_dir}/coverage_report",
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--llvm_profdata",
+        required=False,
+        default="llvm-profdata",
+        help="llvm-profdata executable path",
     )
+    parser.add_argument(
+        "-c",
+        "--llvm_cov",
+        required=False,
+        default="llvm-cov",
+        help="llvm-cov executable path",
+    )
+    args = parser.parse_args()
+    generate_coverage_report(args.llvm_profdata, args.llvm_cov)
+
