d282a370a583d8eaa5df0d89268ca8b10abc10f1
Adam Brzakala <adam.brz@onet.eu>
2023-03-16 20:54:09 +0100

    Revert "Move constructors to header file"
    This reverts commit 2c19b12273493d6f40a8718c8371589ebdeeadb8.

--- libs/bigint/src/BigInt.cpp
+++ libs/bigint/src/BigInt.cpp
@@ -11,6 +11,16 @@
 namespace yabil::bigint
 {
 
+BigInt::BigInt(const std::vector<bigint_base_t> &raw_data, Sign sign) : data(raw_data), sign(sign)
+{
+    normalize();
+}
+
+BigInt::BigInt(std::vector<bigint_base_t> &&raw_data, Sign sign) : data(raw_data), sign(sign)
+{
+    normalize();
+}
+
 BigInt::BigInt(const std::string_view &str, int base)
 {
     sign = (str.front() == '-') ? Sign::Minus : Sign::Plus;
