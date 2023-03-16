744666c46d70f1687a7e15a588f06195701d3b94
Adam Brzakala <adam.brz@onet.eu>
2023-03-16 21:01:34 +0100

    Check why coverage is not working anymore

--- libs/bigint/test/BigIntConversion_tests.cpp
+++ libs/bigint/test/BigIntConversion_tests.cpp
@@ -18,12 +18,6 @@ TEST_F(BigIntConversionTest, canConvertBackToLongInt)
     ASSERT_EQ(61231323ULL, big_uint.to_int());
 }
 
-TEST_F(BigIntConversionTest, canConvertMaxValueBackToLongInt)
-{
-    const BigInt big_int_max_int64(std::numeric_limits<int64_t>::max());
-    ASSERT_EQ(std::numeric_limits<int64_t>::max(), big_int_max_int64.to_int());
-}
-
 TEST_F(BigIntConversionTest, canConvertMinAndMaxValuesBackToUnsignedLongInt)
 {
     const BigInt big_int_min_uint64(std::numeric_limits<uint64_t>::min());
