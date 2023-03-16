5250a2acb94050cba0e0a8c1e239d82530b41b87
Adam Brzakala <adam.brz@onet.eu>
2023-03-16 21:48:19 +0100

    Fix coverage parsing results

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -8,7 +8,7 @@ from build import conan_install, cmake_config, cmake_build
 from github_tools import GitHubLogger
 
 
-def generate_coverage_report(llvm_profdata, llvm_cov):
+def generate_coverage_report(llvm_profdata, llvm_cov, output_line_coverage_percent):
     source_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     build_type = "Debug"
     build_dir = f"{source_dir}/build/coverage"
@@ -24,7 +24,14 @@ def generate_coverage_report(llvm_profdata, llvm_cov):
     output_dir = f"{build_dir}/coverage_report"
 
     merge_raw_profiles(llvm_profdata, binary_dir)
-    generate_html(llvm_cov, source_dir, binary_dir, lib_dir, output_dir)
+    generate_html(
+        llvm_cov,
+        source_dir,
+        binary_dir,
+        lib_dir,
+        output_dir,
+        output_line_coverage_percent,
+    )
 
 
 def run_tests(build_dir):
@@ -52,7 +59,9 @@ def merge_raw_profiles(llvm_profdata, binary_dir):
     GitHubLogger.print("::endgroup::")
 
 
-def generate_html(llvm_cov, src_dir, binary_dir, lib_dir, output_dir):
+def generate_html(
+    llvm_cov, src_dir, binary_dir, lib_dir, output_dir, output_line_coverage_percent
+):
     GitHubLogger.print("::group::Generate coverage HTML report")
     source_files = glob.glob(f"{src_dir}/**/.cpp", recursive=True) + glob.glob(
         f"{src_dir}/**/.h", recursive=True
@@ -73,12 +82,23 @@ def generate_html(llvm_cov, src_dir, binary_dir, lib_dir, output_dir):
         "-show-branch-summary=false" if llvm_cov.endswith("14") else ""
     )
 
-    subprocess.check_call(
+    process = subprocess.run(
         f"{llvm_cov} report -show-region-summary=false -show-functions=false {show_branch_summary} "
         f"-instr-profile coverage.profdata {' '.join(object_flags)} {' '.join(source_flags)}",
         shell=True,
         cwd=binary_dir,
+        capture_output=True,
+        check=True,
     )
+
+    print(process.stdout.decode())
+
+    if output_line_coverage_percent is not None:
+        with open(output_line_coverage_percent, "w") as out:
+            stripped_line_percentage = (
+                process.stdout.decode().splitlines()[-1].split()[-1][:-1]
+            )
+            out.write(stripped_line_percentage)
 
     GitHubLogger.print("::endgroup::")
 
@@ -99,7 +119,16 @@ if __name__ == "__main__":
         default="llvm-cov",
         help="llvm-cov executable path",
     )
+    parser.add_argument(
+        "-o",
+        "--output_line_coverage_percent",
+        required=False,
+        default=None,
+        help="Output file for percentage coverage result",
+    )
 
     args = parser.parse_args()
-    generate_coverage_report(args.llvm_profdata, args.llvm_cov)
+    generate_coverage_report(
+        args.llvm_profdata, args.llvm_cov, args.output_line_coverage_percent
+    )
     print("Done")
