f4ac8d020bdc287ca7f8f5d83609e3591e1350c0
Adam Brzakala <adam.brz@onet.eu>
2023-03-25 19:53:20 +0100

    Replace missing bit_cast on macos by normal casting

--- libs/crypto/src/Utils.cpp
+++ libs/crypto/src/Utils.cpp
@@ -35,12 +35,12 @@ uint64_t log2_int(const yabil::bigint::BigInt &number)
 double log2(const yabil::bigint::BigInt &number)
 {
     constexpr auto item_size_bits = sizeof(yabil::bigint::bigint_base_t) * 8;
-    constexpr auto result_iter_count = 64 / item_size_bits;
+    constexpr int result_iter_count = 64 / item_size_bits;
 
     const auto bit_shift_quotient_removal = std::countl_zero(number.raw_data().back()) + 1;
     uint64_t raw_fraction = 0;
 
-    for (int i = 0; i < result_iter_count && i < number.raw_data().size(); ++i)
+    for (int i = 0; i < result_iter_count && i < static_cast<int>(number.raw_data().size()); ++i)
     {
         const uint64_t fraction_part = static_cast<uint64_t>(number.raw_data()[number.raw_data().size() - 1 - i])
                                        << bit_shift_quotient_removal;
@@ -48,7 +48,8 @@ double log2(const yabil::bigint::BigInt &number)
     }
 
     raw_fraction = (raw_fraction >> 12) | 0x3ff0000000000000;
-    return static_cast<double>(log2_int(number)) + std::log2(std::bit_cast<double>(raw_fraction));
+    const double remaining_part = *static_cast<double *>(reinterpret_cast<void *>(&raw_fraction));
+    return static_cast<double>(log2_int(number)) + std::log2(remaining_part);
 }
 
 double log(const yabil::bigint::BigInt &number, const yabil::bigint::BigInt &base)
