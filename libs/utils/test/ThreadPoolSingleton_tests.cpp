17f0f24a48c1e28a3b2dbbb04369c56bda0914a2
Andrew2a1 <adam.brz@onet.eu>
2023-07-04 14:34:33 +0200

    Add tests for ThreadPoolSingleton

---
+++ libs/utils/test/ThreadPoolSingleton_tests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <yabil/utils/ThreadPoolSingleton.h>
+
+#include <atomic>
+
+using namespace yabil::utils;
+
+class ThreadPoolSingleton_tests : public ::testing::Test
+{
+};
+
+TEST_F(ThreadPoolSingleton_tests, createsOnlyOneInstance)
+{
+    auto &pool1 = ThreadPoolSingleton::instance();
+    auto &pool2 = ThreadPoolSingleton::instance();
+    ASSERT_EQ(&pool1, &pool2);
+}
