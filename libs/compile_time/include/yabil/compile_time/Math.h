256a3302ea4241e7e501edca87cc0d42a16106ec
andrew2a1 <adam.brz@onet.eu>
2024-08-02 16:39:27 +0200

    WIP string conversion

--- libs/compile_time/include/yabil/compile_time/Math.h
+++ libs/compile_time/include/yabil/compile_time/Math.h
@@ -8,13 +8,24 @@
 namespace yabil::compile_time::math
 {
 
-/// @brief Function for calculating the power of a bit integer number in compile time.
+/// @brief Calculate power of a big integer number in compile time.
 /// @tparam Pow The power to which the number is raised.
 /// @tparam NumberSign The sign of the number.
 /// @tparam NumberSize Number of digits in the number.
 /// @tparam NumberData The data of the number.
+/// @returns The result of the power operation.
 template <std::size_t Pow, Sign NumberSign, std::size_t NumberSize, BigIntData<NumberSize> NumberData>
 consteval auto pow();
+
+/// @brief Calculate integer part of binary logarithm of a big integer number in compile time.
+/// @tparam NumberSize Number of digits in the number.
+/// @tparam NumberData The data of the number.
+/// @returns Integer part of binary logarithm result.
+template <std::size_t NumberSize, BigIntData<NumberSize> NumberData>
+consteval uint64_t log2_int();
+
+template <std::size_t NumberSize, BigIntData<NumberSize> NumberData>
+consteval double log2();
 
 }  // namespace yabil::compile_time::math
 
