a4b2c34bb3be1a343408c2ea417b61be320de55d
Adam Brzakala <adam.brz@onet.eu>
2024-08-15 14:53:17 +0200

    Remove comments

--- libs/bigint/src/add_sub/asm/AddSubAsm.cpp
+++ libs/bigint/src/add_sub/asm/AddSubAsm.cpp
@@ -14,9 +14,7 @@ void add_arrays(const bigint_base_t *a, std::size_t a_size, const bigint_base_t 
 {
     static_assert(sizeof(bigint_base_t) == 8, "This implementation requires 8 bytes per bigint digit");
     assert(a_size >= b_size);
-
     asm_add_arrays(a, a_size, b, b_size, r);
-    // add_plain_arrays(a, a_size, b, b_size, r);
 }
 
 void sub_arrays(const bigint_base_t *a, std::size_t a_size, const bigint_base_t *b, std::size_t b_size,
@@ -24,9 +22,7 @@ void sub_arrays(const bigint_base_t *a, std::size_t a_size, const bigint_base_t 
 {
     static_assert(sizeof(bigint_base_t) == 8, "This implementation requires 8 bytes per bigint digit");
     assert(a_size >= b_size);
-
     asm_sub_arrays(a, a_size, b, b_size, r);
-    // sub_plain_arrays(a, a_size, b, b_size, r);
 }
 
 }  // namespace yabil::bigint
