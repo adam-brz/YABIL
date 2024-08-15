d72c3b34a36dca0ea7ccf519dd916169869bbe45
Adam Brzakala <adam.brz@onet.eu>
2024-08-15 18:26:25 +0200

    Use intrinsics in add/sub

--- libs/bigint/test/BigIntConstructor_tests.cpp
+++ libs/bigint/test/BigIntConstructor_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <yabil/bigint/BigInt.h>
+#include <yabil/bigint/BigIntBase.h>
 
 using namespace yabil::bigint;
 
@@ -63,8 +64,9 @@ TEST_F(BigIntConstructorTest, longStringToBigIntWithSign)
     if constexpr (sizeof(bigint_base_t) == 4)
     {
         const BigInt big_int1("-1424112908491024712973012389");
-        const std::vector<bigint_base_t> expected = {0b10100101011000011100110110100101,
-                                                     0b01110110011010000110110111110110, 0b100100110011111111110100011};
+        const std::vector<bigint_base_t> expected = {static_cast<bigint_base_t>(0b10100101011000011100110110100101),
+                                                     static_cast<bigint_base_t>(0b01110110011010000110110111110110),
+                                                     static_cast<bigint_base_t>(0b100100110011111111110100011)};
         EXPECT_EQ(Sign::Minus, big_int1.get_sign());
         EXPECT_EQ(expected, big_int1.raw_data());
     }
@@ -85,7 +87,8 @@ TEST_F(BigIntConstructorTest, longStringInBase2ToBigIntWithSign)
     if constexpr (sizeof(bigint_base_t) == 4)
     {
         const BigInt big_int1("-101010110111100101110101010101010101010", 2);
-        const std::vector<bigint_base_t> expected = {0b10111100101110101010101010101010, 0b1010101};
+        const std::vector<bigint_base_t> expected = {static_cast<bigint_base_t>(0b10111100101110101010101010101010),
+                                                     static_cast<bigint_base_t>(0b1010101)};
         EXPECT_EQ(Sign::Minus, big_int1.get_sign());
         EXPECT_EQ(expected, big_int1.raw_data());
     }
@@ -106,7 +109,9 @@ TEST_F(BigIntConstructorTest, longStringInBase16ToBigIntWithSign)
     if constexpr (sizeof(bigint_base_t) == 4)
     {
         const BigInt big_int1("+abcd18782172918aafffbab", 16);
-        const std::vector<bigint_base_t> expected = {0xaafffbabULL, 0x82172918ULL, 0xabcd187ULL};
+        const std::vector<bigint_base_t> expected = {static_cast<bigint_base_t>(0xaafffbabULL),
+                                                     static_cast<bigint_base_t>(0x82172918ULL),
+                                                     static_cast<bigint_base_t>(0xabcd187ULL)};
         EXPECT_EQ(Sign::Plus, big_int1.get_sign());
         EXPECT_EQ(expected, big_int1.raw_data());
     }
