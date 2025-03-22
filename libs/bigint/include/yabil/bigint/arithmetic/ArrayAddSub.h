1a3bcdaea7bdc7868d686bbb0a2aa1a7fc019590
Adam Brzakala <adam.brz@onet.eu>
2025-03-22 01:02:37 +0100

    Update documentation tools

--- libs/bigint/include/yabil/bigint/arithmetic/ArrayAddSub.h
+++ libs/bigint/include/yabil/bigint/arithmetic/ArrayAddSub.h
@@ -26,7 +26,7 @@ YABIL_BIGINT_EXPORT void add_arrays_with_carry(const bigint_base_t *a, std::size
 /// @param b Pointer to first item of number b.
 /// @param b_size Size in elements of array with start at b.
 /// @param r output pointer for array to store difference. Output array must have size greater or equal a_size.
-/// @param borrot Additional borrow to begin subtraction with.
+/// @param borrow Additional borrow to begin subtraction with.
 YABIL_BIGINT_EXPORT void sub_arrays_with_borrow(const bigint_base_t *a, std::size_t a_size, const bigint_base_t *b,
                                                 std::size_t b_size, bigint_base_t *r, bigint_base_t borrow = 0);
 
--- libs/parallel/include/yabil/parallel/ThreadPool.h
+++ libs/parallel/include/yabil/parallel/ThreadPool.h
@@ -11,7 +11,8 @@
 namespace yabil::parallel
 {
 
-/// @brief Thread pool implementing task submission
+/// @brief Thread pool allowing submitting tasks for asynchronous execution.
+/// @details Thread pool cannot be copied, but can be moved.
 class ThreadPool
 {
 private:
@@ -20,19 +21,23 @@ private:
 
 public:
     /// @brief Creates thread pool and starts given number of threads.
-    /// @param concurrency Number of threads to use with pool. Value <= 0 means automatic detection of available cores
+    /// @param threads_number Number of threads to use with pool. Value <= 0 means automatic detection of available
+    /// cores
     YABIL_PARALLEL_EXPORT explicit ThreadPool(int threads_number = 0);
     YABIL_PARALLEL_EXPORT ~ThreadPool();
 
     ThreadPool(const ThreadPool &) = delete;
     ThreadPool(ThreadPool &) = delete;
 
-    /// @brief Submit task for execution. Task will be executed as soon as free thread is available
+    /// @brief Submit task for execution. Task will be executed as soon as free thread is available.
+    /// @details Task can be any callable object (see <tt>std::invocable</tt>).
+    /// If this method is called from internal pool thread (from this thread pool),
+    /// then task is executed synchronously to avoid deadlocks.
     /// @tparam FunctionType Type of function to submit
     /// @param func Function to submit
     /// @return \p std::future for obtaining function execution results
     template <std::invocable FunctionType>
-    auto submit(FunctionType &&func)
+    auto submit(FunctionType &&func) -> std::future<std::invoke_result_t<FunctionType>>
     {
         using ReturnType = std::invoke_result_t<FunctionType>;
         auto task = std::make_shared<std::packaged_task<ReturnType()>>(std::forward<FunctionType>(func));
@@ -41,12 +46,12 @@ public:
         return future;
     }
 
-    /// @brief Get thread count.
+    /// @brief Get thread count for this thread pool.
     /// @return Number of threads in \p ThreadPool
     YABIL_PARALLEL_EXPORT std::size_t thread_count() const;
 
     /// @brief Check if threads are active.
-    /// @details Threads are active if they are running and are not scheduled t stop.
+    /// @details Threads are active if they are running and are not scheduled to stop.
     /// @return \p true if threads are active and \p false otherwise
     YABIL_PARALLEL_EXPORT bool is_active() const;
 
@@ -60,10 +65,13 @@ public:
     /// @return \p true if threads were started successfully and \p false if they are already running.
     YABIL_PARALLEL_EXPORT bool start(int threads_number);
 
-    /// @brief Stop all threads from the pool.
+    /// @brief Schedule all threads from the pool to stop.
+    /// @details Threads should stop work as soon as they finish their current task.
+    /// This method returns immediately.
+    /// To wait until all threads are stopped use @p ThreadPool::wait_stopped method.
     YABIL_PARALLEL_EXPORT void stop();
-    ///
-    /// @brief Wait until all threads are stopped.
+    
+    /// @brief Schedule all threads to stop and wait until all are stopped.
     YABIL_PARALLEL_EXPORT void wait_stopped();
 
 private:
