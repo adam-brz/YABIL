0a2f774b7dfd6650f2ef42125c60a023d8b02607
Adam Brzakala <adam.brz@onet.eu>
2025-03-24 21:44:18 +0100

    Update ThreadPool docs and start method

--- libs/parallel/include/yabil/parallel/ThreadPoolSingleton.h
+++ libs/parallel/include/yabil/parallel/ThreadPoolSingleton.h
@@ -16,6 +16,8 @@ public:
     YABIL_PARALLEL_EXPORT static ThreadPool &instance();
 
     /// @brief Sets new thread pool instance to use.
+    /// @details Make sure to run this method only in single threading case,
+    /// calling asynchronously with @p instance method can result in data race condition.
     /// @param new_instance Unique pointer to new instance which will replace old one.
     YABIL_PARALLEL_EXPORT static void reset(std::unique_ptr<ThreadPool> &&new_instance);
 };
